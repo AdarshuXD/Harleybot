b9cfff4784859b42de3aa5a8ecf57b0355ed873d
AdarshuXD <108065687+AdarshuXD@users.noreply.github.com>
2022-06-24 17:27:22 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+"""
+Api hash 4fc0bff1c72b86743d91162fca4e0e43
+Api id 13749404
+Token 5478256107:AAFzGJefXrmvfZa671i_BGMUZtk4oooR508
+"""
+
+API_HASH = os.environ.get("API_HASH")
+API_ID = int(os.environ.get("API_ID")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
